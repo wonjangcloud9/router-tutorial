9fb57107e1c048409c2eb536e2527508e193ed8c
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 16:38:52 +0900

    url parameter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,20 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Link to="/about">About</Link>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/profiles/velopert">velopert</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong</Link>
+        </li>
+        <li>
+          <Link to="/profiles/unknown">unknown</Link>
+        </li>
+      </ul>
     </div>
   );
 };
