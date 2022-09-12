168c3aaf4add4f36fedb31e65b4c0933b9305143
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 16:45:35 +0900

    query string

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,28 @@
+import { useSearchParams } from "react-router-dom";
+
 const About = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get("detail");
+  const mode = searchParams.get("mode");
+
+  const onToogleDetail = () => {
+    setSearchParams({ mode, detail: detail === "true" ? false : true });
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParams({ detail, mode: nextMode });
+  };
   return (
     <div>
-      <h1>About</h1>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToogleDetail}>detail</button>
+      <button onClick={onIncreaseMode}>mode</button>
     </div>
   );
 };
+
 export default About;
