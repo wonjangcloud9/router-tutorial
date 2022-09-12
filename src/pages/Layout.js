8831ed7da2af717441ba7d67870936c85ecbdcf4
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-12 17:01:30 +0900

    useNavigate

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const goArticles = () => {
+    navigate("/articles", { replace: true });
+  };
+
   return (
     <div>
       <header style={{ background: "lightgray", padding: 16, fontSize: 24 }}>
-        Header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}>Articles</button>
       </header>
       <main>
         <Outlet />
